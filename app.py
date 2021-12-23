8552a47fe597d0901fc39480a333c16d2b5fe036
Rakin061 <salmankaderrakin@gmail.com>
2021-12-23 16:18:43 +0600

    designation added

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ A skeleton python script which reads from an input file,
 writes to an output file and parses command line arguments.....
 
 ## Author: Salman Rakin
+Sr. Software Engineer
 """
 from __future__ import print_function
 import sys
